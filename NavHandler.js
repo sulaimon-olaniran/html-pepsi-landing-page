d77a446ec7831ab64514921a9bd91d9c0133d960
Olaniran Sulaimon <suzzwayne@gmail.com>
2020-07-15 02:09:48 +0100

    animates scroll and adds backdrop for naw drawer

--- NavHandler.js
+++ NavHandler.js
@@ -1,7 +1,50 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "70%";
+    document.getElementById("mySidenav").style.width = "70%"
+    document.getElementById('backdrop').style.display = "block"
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById('backdrop').style.display = "none"
 }
+
+
+$('#home-link').on('click', function(){
+    const home = $('#home').position().top
+
+    $('html, body').animate({
+        scrollTop : home
+    }, 1000)
+})
+
+$('#about-link').on('click', function(){
+    const about = $('#about').position().top
+
+    $('html, body').animate({
+        scrollTop : about
+    }, 1000)
+})
+
+$('#products-link').on('click', function(){
+    const products = $('products').position().top
+
+    $('html, body').animate({
+        scrollTop : products
+    }, 1000)
+})
+
+$('#contact-link').on('click', function(){
+    const contact = $('#contact').position().top
+
+    $('html, body').animate({
+        scrollTop : contact
+    }, 1000)
+})
+
+$('#socials-link').on('click', function(){
+    const socials = $('#socials').position().top
+
+    $('html, body').animate({
+        scrollTop : socials
+    }, 1000)
+})
